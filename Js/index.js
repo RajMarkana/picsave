6dba189bea5c7bfb29a392a760d38e8bf7fc1cf4
Raj <rtechcode101@gmail.com>
2022-12-04 11:57:56 +0530

    Add New Functions and Fonts

--- Js/index.js
+++ Js/index.js
@@ -1,38 +1,38 @@
 $(document).ready(function () {
-   
-    
-    $("#totalimg").hide();
-   
-    var i =0;
-   
-    $("#black").click(function (e) { 
-     
-        if(i == 0){
 
-            $("body").css("background-color","#232b30");
+
+    $("#totalimg").hide();
+
+    var i = 0;
+
+    $("#black").click(function (e) {
+
+        if (i == 0) {
+
+            $("body").css("background-color", "#232b30");
             $(".search_container").css("color", "white");
             $(".bw").css("color", "whitesmoke");
             $(".theme").find("img").attr("src", "icon/light.png");
-            i =1;
-         
+            i = 1;
+
         }
-        else{
-            $("body").css("background-color","whitesmoke");
+        else {
+            $("body").css("background-color", "whitesmoke");
             $(".search_container").css("color", "black");
             $(".bw").css("color", "grey");
 
             $(".theme, img").find("img").attr("src", "icon/dark.png");
 
 
-            i=0;
-           
+            i = 0;
+
         }
 
-        
-    }); 
+
+    });
 });
-$("#search").keypress(function(e) {
-    if(e.which == 13) {
+$("#search").keypress(function (e) {
+    if (e.which == 13) {
         SearchPhoto(1);
     }
 });
@@ -42,54 +42,54 @@ var pgno = 1;
 
 function SearchPhoto(pgno1) {
 
-    if(pgno1 == 1){
+    if (pgno1 == 1) {
         pgno = 1;
     }
     $('#topscroll').click();
     $("#pageno").html(pgno);
 
-   
-    console.log(pgno+"pg")
+
+    console.log(pgno + "pg")
 
     $("#images").empty();
-    if ($('#images').is(':empty')){
-           
-           
-        
-            let empty = `
+    if ($('#images').is(':empty')) {
+
+
+
+        let empty = `
             <h1 id="msg">Loading...</h1>
         `
-        $("#images").append(empty);   
+        $("#images").append(empty);
         $(".prevnext").hide();
-        
+
     }
 
 
     let id = "eI4ensPee7LiZ2boqvI_aFgzXRFdjyBTDJnClUEVkzI"
     let search = $("#search").val();
-    let url = "https://api.unsplash.com/search/photos/?query="+search+"&client_id="+id+"&page="+pgno+"&per_page=30";
-   
+    let url = "https://api.unsplash.com/search/photos/?query=" + search + "&client_id=" + id + "&page=" + pgno + "&per_page=30";
+
     fetch(url)
-    .then(function(data){
-        return data.json();
+        .then(function (data) {
+            return data.json();
 
-    })
-    .then(function(data){
-        console.log(data);
+        })
+        .then(function (data) {
+            console.log(data);
 
-        if (data.total == 0) {
-        $("#images").empty();
+            if (data.total == 0) {
+                $("#images").empty();
 
-        }
-        else{
-        $("#images").empty();
-            
-            
-                    data.results.forEach(photo => {
-                        
-                        $("#totalimg").show();
-            
-                        let result =`
+            }
+            else {
+                $("#images").empty();
+
+
+                data.results.forEach(photo => {
+
+                    $("#totalimg").show();
+
+                    let result = `
                         
                         <div class="pic">
 
@@ -101,112 +101,111 @@ function SearchPhoto(pgno1) {
                          
                         </div>
                         `
-                        $("#images").append(result);
+                    $("#images").append(result);
 
-                        //image url regular to full+ &w=700&h=300
-                        // download link midify as above
+                    //image url regular to full+ &w=700&h=300
+                    // download link midify as above
 
-                        
-                        
-                        
-                    });
-                    $(".prevnext").show();
-                    $("#totalimg").show();
-                    $("#totalimg").html("Showing Results : "+data.total);
-                     window.total_page = data.total_pages;
-        
-                   
-                     if (pgno == total_page){
-                        $("#next").hide();
-                    }
-                    else{
-                        $("#next").show();
 
-                    }
-                    if (pgno == 1)
-                    {
-                        $("#prev").hide();
 
-                    }
-                    else{
-                        $("#prev").show();
 
-                    }
-                
+                });
+                $(".prevnext").show();
+                $("#totalimg").show();
+                $("#totalimg").html("Showing Results : " + data.total);
+                window.total_page = data.total_pages;
 
-                
-              
 
-                    
-        }
-       
+                if (pgno == total_page) {
+                    $("#next").hide();
+                }
+                else {
+                    $("#next").show();
 
-        if ($('#images').is(':empty')){
-           
-            $("#totalimg").hide();
-           
-            if (search == ""){
-                let empty = `
+                }
+                if (pgno == 1) {
+                    $("#prev").hide();
+
+                }
+                else {
+                    $("#prev").show();
+
+                }
+
+
+
+
+
+
+            }
+
+
+            if ($('#images').is(':empty')) {
+
+                $("#totalimg").hide();
+
+                if (search == "") {
+                    let empty = `
                 <h1 id="msg" class="bw">Write Someting to Search...!😕</h1>
                 
                 `
-                $("#images").append(empty);     
-            }
-            else{
+                    $("#images").append(empty);
+                }
+                else {
 
-                let empty = `
-                    <h1 id="msg" class="bw">`+ search +` Not Found...!🧐</h1>
+                    let empty = `
+                    <h1 id="msg" class="bw">`+ search + ` Not Found...!🧐</h1>
                 `
-                $("#images").append(empty);
-                
+                    $("#images").append(empty);
+
+                }
+                $(".prevnext").hide();
+
             }
-            $(".prevnext").hide();
 
-        }
-    
 
-        $(".pic a").hide();
-        $(".pic .author").hide();
-        $(".pic").hover(function (e) { 
-        $(".pic a").hide();
-        $(".pic .author").hide();
+            $(".pic a").hide();
+            $(".pic .author").hide();
+            $(".pic").hover(function (e) {
+                $(".pic a").hide();
+                $(".pic .author").hide();
 
-            $(this).find("a").fadeIn();
-            $(this).find(".author").fadeIn();
+                $(this).find("a").fadeIn();
+                $(this).find(".author").fadeIn();
 
-            
-        });
 
-        
-    })
-    
-    
-    
+            });
+
+
+        })
+
+
+
 }
-$("#next").click(function (e) { 
- 
+$("#next").click(function (e) {
 
-    if (pgno<=total_page){
 
-      
-            pgno++;
-            SearchPhoto(pgno)
-            
+    if (pgno <= total_page) {
+
+
+        pgno++;
+        SearchPhoto(pgno)
+
 
     }
 
 });
-$("#prev").click(function (e) { 
+$("#prev").click(function (e) {
 
-    if (pgno>=1){
+    if (pgno >= 1) {
 
-     
+
         pgno--;
         SearchPhoto(pgno)
-        
+
 
 
     }
-    
+
 });
 $("#topscroll").hide();
--- Js/random.js
+++ Js/random.js
@@ -1,47 +1,47 @@
 $(document).ready(function () {
-    
-    var i =0;
-    $("#black").click(function (e) { 
-     
-        if(i == 0){
 
-            $("body").css("background-color","#232b30");
+    var i = 0;
+    $("#black").click(function (e) {
+
+        if (i == 0) {
+
+            $("body").css("background-color", "#232b30");
             $(".search_container").css("color", "white");
             $(".bw").css("color", "whitesmoke");
             $(".theme").find("img").attr("src", "icon/light.png");
-            i =1;
-         
+            i = 1;
+
         }
-        else{
-            $("body").css("background-color","whitesmoke");
+        else {
+            $("body").css("background-color", "whitesmoke");
             $(".search_container").css("color", "black");
             $(".bw").css("color", "grey");
 
             $(".theme, img").find("img").attr("src", "icon/dark.png");
 
 
-            i=0;
-           
+            i = 0;
+
         }
 
-        
-    }); 
+
+    });
 
     function callpic() {
-            
-            
+
+
         let id = "eI4ensPee7LiZ2boqvI_aFgzXRFdjyBTDJnClUEVkzI"
-        let url = "https://api.unsplash.com/photos/random/?&client_id="+id+"";
+        let url = "https://api.unsplash.com/photos/random/?&client_id=" + id + "";
 
         fetch(url)
-        .then(function(data){
-            return data.json();
+            .then(function (data) {
+                return data.json();
 
-        })
-        .then(function(data){
-            console.log(data);
+            })
+            .then(function (data) {
+                console.log(data);
 
-            let result = `
+                let result = `
             <img src="${data.links.download}"' id='random_img'><br>
             <div class="random_img_option">
 
@@ -52,26 +52,26 @@ $(document).ready(function () {
 
             </div>
             `;
-            $(".random_image").empty(); 
-            $(".random_image").html(result);
-        })
+                $(".random_image").empty();
+                $(".random_image").html(result);
+            })
     }
 
     callpic()
 
-        
-        $("#random_btn").on("click", function(){ callpic();});
 
-        $(".random_image").on("click", "#random_btn", function(){
-    callpic();
-          }); 
+    $("#random_btn").on("click", function () { callpic(); });
 
-        
-        
+    $(".random_image").on("click", "#random_btn", function () {
+        callpic();
+    });
 
-       
-        
- 
+
+
+
+
+
+
 
 
 
